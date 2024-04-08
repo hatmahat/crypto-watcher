8e490d3de90b3d7a3d1151ccca3686b4468d00d7
hatmahat <mahatma.wisesa@sinarmasmining.com>
2024-04-08 16:13:53 +0700

    [REMOVE]] http param on RunWorker

--- internal/app/worker.go
+++ internal/app/worker.go
@@ -15,7 +15,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func RunWorker(httpServer *http.Server, cfg *config.Config) {
+func RunWorker(cfg *config.Config) {
 	ctx := context.Background()
 	httpClient := http.Client{
 		Timeout: time.Duration(cfg.WorkerConfig.GlobalTimeout) * time.Millisecond,
