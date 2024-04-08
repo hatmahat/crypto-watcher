dcb1b05fb18c6e0c6e5f02b74903118ad61ddfa4
Mahatma Wisesa <mahatma.wisesa@sinarmasmining.com>
2024-04-08 15:10:47 +0700

    [ADD] worker setup (#4)

---
+++ internal/init_module/worker.go
@@ -0,0 +1,31 @@
+package init_module
+
+import (
+	"crypto-watcher-backend/internal/worker"
+	"fmt"
+)
+
+type (
+	// WorkerWrapper wraps a Worker module with its gracful handler
+	WorkerWrapper struct {
+		Cron *worker.WatcherWorker
+	}
+
+	WorkerGracefulHandler struct{}
+)
+
+// NewWorkerWrapper creates a new Worker wrapper instance.
+func NewWorkerWrapper(cron *worker.WatcherWorker) *WorkerWrapper {
+	return &WorkerWrapper{
+		Cron: cron,
+	}
+}
+
+func NewWorkerGracefulHandler() *WorkerGracefulHandler {
+	return &WorkerGracefulHandler{}
+}
+
+func (h *WorkerGracefulHandler) ReleaseResource() {
+	fmt.Println("Releasing worker resources...")
+	defer fmt.Println("Worker resources are gracefully released")
+}
---
+++ internal/worker/init.go
@@ -0,0 +1,85 @@
+package worker
+
+import (
+	"context"
+	"crypto-watcher-backend/internal/config"
+	"fmt"
+
+	"github.com/robfig/cron/v3"
+)
+
+type (
+	WatcherWorker struct {
+		cronPool *cron.Cron
+		config   *config.Config
+		cronJob  []worker
+	}
+
+	WatcherWorkerParam struct {
+		Config *config.Config
+	}
+
+	JobParameter struct {
+		Name     string
+		TimeSpec string
+		Handler  func(ctx context.Context)
+	}
+
+	worker interface {
+		GenerateWorkerParameters() []JobParameter
+	}
+)
+
+func NewWatcherWorker(param WatcherWorkerParam) *WatcherWorker {
+	cronPool := cron.New(
+		cron.WithParser(
+			cron.NewParser(cron.Second | cron.Minute | cron.Hour | cron.Dom | cron.Month | cron.Dow | cron.Descriptor),
+		),
+	)
+
+	c := WatcherWorker{
+		cronPool: cronPool,
+		config:   param.Config,
+	}
+
+	c.cronJob = []worker{
+		// TODO: Add new worker here
+	}
+
+	c.registerJobs()
+
+	return &c
+}
+
+func (c *WatcherWorker) registerJobs() {
+	fmt.Println("Registering jobs...")
+	defer fmt.Println("Jobs are successfully registered")
+
+	for i := range c.cronJob {
+		for _, cronJob := range c.cronJob[i].GenerateWorkerParameters() {
+			_, err := c.cronPool.AddFunc(cronJob.TimeSpec, c.GenerateHandler(cronJob.Handler))
+			if err != nil {
+				// TODO: Add logger here
+			}
+		}
+	}
+}
+
+func (c *WatcherWorker) GenerateHandler(f func(ctx context.Context)) func() {
+	return func() {
+		f(context.Background())
+	}
+}
+
+// Start starts all the registered jobs
+func (c *WatcherWorker) Start() {
+	fmt.Println("Starting worker...")
+	c.cronPool.Start()
+}
+
+// Stop stops all currently running jobs
+func (c *WatcherWorker) Stop() {
+	fmt.Println("Stopping worker...")
+	defer fmt.Println("Cron is gracefully stopped")
+	c.cronPool.Stop()
+}
