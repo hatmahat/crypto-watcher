f458e3ac5ef713193eafa6e225742e79d17855a0
Mahatma Wisesa <mahatma.wisesa@sinarmasmining.com>
2024-04-14 22:47:09 +0700

    [UPDATE] migration to capture asset type (#20)
    * [UPDATE] migration to capture asset type
    
    * [UPDATE] Makefile

--- migration/deploy/1713025849_init_crypto_watcher_schema.sql
+++ migration/deploy/1713025849_init_crypto_watcher_schema.sql
@@ -5,53 +5,54 @@ BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     uuid UUID DEFAULT uuid_generate_v4(),
     username VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     phone_number VARCHAR(50) UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE currency_rates (
-    rate_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     currency_pair VARCHAR(7) DEFAULT 'USD_IDR',
     rate DECIMAL(18, 4),
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE bitcoin_prices (
-    price_id SERIAL PRIMARY KEY,
+CREATE TABLE asset_prices (
+    id SERIAL PRIMARY KEY,
+    asset_type VARCHAR(50), -- 'CRYPTO' or 'STOCK'
+    asset_code VARCHAR(50), -- such as 'BTC', 'ETH' for cryptos or 'AAPL', 'GOOGL' for stocks
     price_usd DECIMAL(18, 4),
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE user_preferences (
-    preference_id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(user_id),
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    asset_type VARCHAR(50), -- 'CRYPTO' or 'STOCK'
+    asset_code VARCHAR(50), -- such as 'BTC', 'ETH' for cryptos or 'AAPL', 'GOOGL' for stocks
     threshold_percentage DECIMAL(5, 2),
-    observation_period INTEGER DEFAULT 15,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+    observation_period INTEGER DEFAULT 15, -- This is in minutes
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE notifications (
-    notification_id SERIAL PRIMARY KEY,
-    notification_type VARCHAR(255),
-    user_id INTEGER REFERENCES users(user_id),
-    price_id INTEGER REFERENCES bitcoin_prices(price_id),
-    parameters JSONB DEFAULT '{}',
-    created_at TIMESTAMP DEFAULT NOW()
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    preference_id INTEGER REFERENCES user_preferences(id), -- Reference to user preferences
+    message TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX IF NOT EXISTS idx_users_uuid ON users USING btree (uuid);
 CREATE INDEX IF NOT EXISTS idx_users_username ON users USING btree (username);
 CREATE INDEX IF NOT EXISTS idx_currency_rates_pair_time ON currency_rates USING btree (currency_pair, created_at DESC);
-CREATE INDEX IF NOT EXISTS idx_bitcoin_prices_created_at ON bitcoin_prices USING btree (created_at DESC);
+CREATE INDEX IF NOT EXISTS idx_asset_prices_created_at ON asset_prices USING btree (asset_type, asset_code, created_at DESC);
 CREATE INDEX IF NOT EXISTS idx_user_preferences_user_id ON user_preferences USING btree (user_id);
-CREATE INDEX IF NOT EXISTS idx_notifications_user_id ON notifications USING btree (user_id);
-CREATE INDEX IF NOT EXISTS idx_notifications_price_id ON notifications USING btree (price_id);
-CREATE INDEX IF NOT EXISTS idx_notifications_parameters ON notifications USING gin (parameters);
+CREATE INDEX IF NOT EXISTS idx_notifications_user_preference_id ON notifications USING btree (preference_id);
 
 COMMIT;
--- migration/revert/1713025849_init_crypto_watcher_schema.sql
+++ migration/revert/1713025849_init_crypto_watcher_schema.sql
@@ -2,18 +2,16 @@
 
 BEGIN;
 
-DROP INDEX IF EXISTS idx_notifications_parameters;
-DROP INDEX IF EXISTS idx_notifications_price_id;
-DROP INDEX IF EXISTS idx_notifications_user_id;
+DROP INDEX IF EXISTS idx_notifications_user_preference_id;
 DROP INDEX IF EXISTS idx_user_preferences_user_id;
-DROP INDEX IF EXISTS idx_bitcoin_prices_created_at;
+DROP INDEX IF EXISTS idx_asset_prices_created_at;
 DROP INDEX IF EXISTS idx_currency_rates_pair_time;
 DROP INDEX IF EXISTS idx_users_username;
 DROP INDEX IF EXISTS idx_users_uuid;
 
 DROP TABLE IF EXISTS notifications;
 DROP TABLE IF EXISTS user_preferences;
-DROP TABLE IF EXISTS bitcoin_prices;
+DROP TABLE IF EXISTS asset_prices;
 DROP TABLE IF EXISTS currency_rates;
 DROP TABLE IF EXISTS users;
 
