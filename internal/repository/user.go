74b8b4bd918a309e25c91491f80102365a673464
Mahatma Wisesa <hatmahat@gmail.com>
2024-04-27 18:49:05 +0700

    [ADD] preference type on query (#40)

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -23,14 +23,15 @@ type (
 	}
 
 	GetUserFilter struct {
-		ReportTime string
-		AssetType  string
-		AssetCode  string
+		ReportTime     string
+		AssetType      string
+		AssetCode      string
+		PreferenceType string
 	}
 )
 
 const (
-	getUserByReportTimeQuery = `SELECT u.id, u.telegram_chat_id, up.id preference_id FROM users u INNER JOIN user_preferences up ON up.user_id = u.id WHERE up.report_time = $1 AND asset_type = $2 AND asset_code = $3`
+	getUserByReportTimeQuery = `SELECT u.id, u.telegram_chat_id, up.id preference_id FROM users u INNER JOIN user_preferences up ON up.user_id = u.id WHERE up.report_time = $1 AND asset_type = $2 AND asset_code = $3 AND preference_type = $4`
 )
 
 func NewUserRepo(param UserRepoParam) UserRepo {
@@ -45,7 +46,7 @@ func (ur *userRepo) GetUserAndUserPreferenceByReportTime(ctx context.Context, fi
 		result []helper.UserAndUserPreference
 		err    error
 	)
-	err = ur.db.Select(&result, getUserByReportTimeQuery, filter.ReportTime, filter.AssetType, filter.AssetCode)
+	err = ur.db.Select(&result, getUserByReportTimeQuery, filter.ReportTime, filter.AssetType, filter.AssetCode, filter.PreferenceType)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
 			"err":         err.Error(),
--- internal/service/crypto_report.go
+++ internal/service/crypto_report.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto-watcher-backend/internal/constant/asset_const"
 	"crypto-watcher-backend/internal/constant/notification_const"
+	"crypto-watcher-backend/internal/constant/user_preference_const"
 	"crypto-watcher-backend/internal/entity"
 	"crypto-watcher-backend/internal/entity/helper"
 	"crypto-watcher-backend/internal/repository"
@@ -17,11 +18,12 @@ import (
 func (cs *cryptoService) dailyBitcoinPriceReport(ctx context.Context, bitcoinPriceUSD, rateUSDToIDR *int) {
 	const funcName = "[internal][service]DailyBitcoinPriceReport"
 
-	// TODO (improvement): not only support bitcoin, get from user preference
+	// TODO (improvement): not only support bitcoin, get it from user preference
 	getUserFilter := repository.GetUserFilter{
-		ReportTime: format.GetSimpleTime(),
-		AssetType:  asset_const.CRYPTO,
-		AssetCode:  asset_const.BTC,
+		ReportTime:     format.GetSimpleTime(),
+		AssetType:      asset_const.CRYPTO,
+		AssetCode:      asset_const.BTC,
+		PreferenceType: user_preference_const.DailyReport,
 	}
 	users, err := cs.userRepo.GetUserAndUserPreferenceByReportTime(ctx, getUserFilter) // TODO (improvement): make it effective so it won't query every minute (chaching)
 	if err != nil {
