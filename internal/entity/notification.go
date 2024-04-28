e6af4237eb1cc43d5f883a5c8c664ad6667cf512
Mahatma Wisesa <hatmahat@gmail.com>
2024-04-28 21:19:17 +0700

    [CHORE] add price checkpoint on user_preference (#43)

--- internal/entity/notification.go
+++ internal/entity/notification.go
@@ -2,13 +2,13 @@ package entity
 
 type Notification struct {
 	BaseEntity
-	UserId       int64      `db:"user_id"`
-	PreferenceId int64      `db:"preference_id"`
-	Status       string     `db:"status"`
-	Parameters   Parameters `db:"parameters"`
+	UserId       int64     `db:"user_id"`
+	PreferenceId int64     `db:"preference_id"`
+	Status       string    `db:"status"`
+	Metadata     *Metadata `db:"metadata"`
 }
 
-type Parameters struct {
+type Metadata struct {
 	Message  *string `json:"message,omitempty"`
 	Provider *string `json:"provider,omitempty"`
 }
--- internal/entity/user_preference.go
+++ internal/entity/user_preference.go
@@ -6,10 +6,12 @@ type UserPreference struct {
 	BaseEntity
 	UserId              int64      `db:"user_id"`
 	PreferenceType      string     `db:"preference_type"`
+	Operator            *string    `db:"operator"`
 	AssetType           string     `db:"asset_type"`
 	AssetCode           string     `db:"asset_code"`
+	PriceCheckpoint     *float64   `db:"price_checkpoint"`
 	ThresholdPercentage *float64   `db:"threshold_percentage"`
-	ObservationPeriod   int64      `db:"observation_period"`
+	ObservationPeriod   *int64     `db:"observation_period"`
 	ReportTime          *time.Time `db:"report_time"`
 	IsActive            bool       `db:"is_active"`
 }
