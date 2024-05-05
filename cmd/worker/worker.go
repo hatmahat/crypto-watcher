cd120b8517228fef288608e6e595ba5fe29a5fd0
hatmahat <mahatma.wisesa@sinarmasmining.com>
2024-05-05 21:44:14 +0700

    [REMOVE] write and idle timeout

--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -12,9 +12,7 @@ import (
 func Start(cfg *config.Config) {
 	logrus.Info("Starting crypto-watcher worker...")
 	httpServer := http.Server{
-		ReadTimeout:  time.Duration(cfg.WorkerConfig.GlobalTimeout) * time.Microsecond,
-		WriteTimeout: time.Duration(cfg.WorkerConfig.GlobalTimeout) * time.Microsecond,
-		IdleTimeout:  time.Duration(cfg.WorkerConfig.GlobalTimeout) * time.Microsecond,
+		ReadTimeout: time.Duration(cfg.WorkerConfig.GlobalTimeout) * time.Microsecond,
 	}
 	app.RunWorker(&httpServer, cfg)
 }
